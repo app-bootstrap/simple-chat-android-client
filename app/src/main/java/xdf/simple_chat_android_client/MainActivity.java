0791d642d7bd6d393822f4408420b4d636e5fe0d
xudafeng <xudafeng@126.com>
2016-07-03 15:56:26 +0800

    add webview

--- app/src/main/java/xdf/simple_chat_android_client/MainActivity.java
+++ app/src/main/java/xdf/simple_chat_android_client/MainActivity.java
@@ -2,12 +2,31 @@ package xdf.simple_chat_android_client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private static final String TAG = "android-chat-sample";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    public void initView() {
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                return super.onJsAlert(view, url, message, result);
+            }
+        });
+        webView.loadUrl("http://192.168.11.101:8081/app.html?server=ws://192.168.11.101:5678/");
     }
 }
